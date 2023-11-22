75b83ac3514b501c40909d9ec7883a1cc432e488
Augusto Medeiros da Costa <augusto.costa@dev.audaces.local>
2023-11-22 08:44:48 -0300

    feat: add base entity and product entity

---
+++ ecommerce-api/Domain/Entities/Base/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_api.Domain.Entities.Base;
+
+public class EntityBase
+{
+    [Key]
+    public int Id { get; set; }
+}
---
+++ ecommerce-api/Domain/Entities/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿using ecommerce_api.Domain.Entities.Base;
+
+namespace ecommerce_api.Domain.Entities;
+
+public class ProductEntity : EntityBase
+{
+    public string? Name { get; set; }
+    
+    
+}
