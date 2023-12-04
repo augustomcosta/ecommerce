7b078253116beb8cc5ae63d8a7e07dcb041132f7
Augusto Medeiros da Costa <augusto.costa@dev.audaces.local>
2023-12-04 10:55:31 -0300

    feat: create pagination and pager components

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
+import { PagerComponent } from './pager/pager.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagingHeaderComponent,
+    PagerComponent
+  ],
   imports: [
     CommonModule,
     NgbPagination,
   ],
   exports: [
-    NgbPagination
+    NgbPagination,
+    PagingHeaderComponent,
+    PagerComponent
   ]
 })
 
