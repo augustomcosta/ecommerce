35b8348e253b080114da0e838c3017dba5d79db3
Augusto Medeiros da Costa <augusto.costa@dev.audaces.local>
2023-12-05 10:07:32 -0300

    feat: add errors page and toastr notifications

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss'
+})
+export class TestErrorComponent {
+  url = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  get404Error() {
+    this.http.get(this.url + 'products/42').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+    
+  get500Error() {
+    this.http.get(this.url + 'Buggy/servererror').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+    
+  get400Error() {
+    this.http.get(this.url + 'Buggy/badrequest').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.http.get(this.url + 'products/fortytwo').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+
+}
