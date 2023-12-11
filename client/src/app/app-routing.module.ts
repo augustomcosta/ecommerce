b0e85570eb283f2257719bc263a31bcd6386f221
Augusto <augusto.medeirosdacosta@gmail.com>
2023-12-11 14:13:09 -0300

    feat: add header title to basket page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent },
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Basket'}},
   {path: '**', redirectTo: "", pathMatch: 'full'},
 ];
 
