91b9f9a55cc2dca9627bd925eca9496a942e69fc
Augusto Medeiros da Costa <augusto.costa@dev.audaces.local>
2023-12-13 10:48:09 -0300

    feat: create checkout module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,9 +12,10 @@ const routes: Routes = [
   {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
-  {path: 'server-error', component: ServerErrorComponent },
+  {path: 'server-error', component: ServerErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule), data: {breadcrumb: 'Shop'}},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(m => m.BasketModule), data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule), data: {breadcrumb: 'Checkout'}},
   {path: '**', redirectTo: "", pathMatch: 'full'},
 ];
 
