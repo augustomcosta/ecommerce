befee1a155c3d211042f55c6df74c37906146248
Augusto Medeiros da Costa <augusto.costa@dev.audaces.local>
2023-12-13 08:20:59 -0300

    old changes

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +14,10 @@ export class ProductDetailsComponent implements OnInit {
   
   product?: Product;
   
-  constructor(private shopService : ShopService, private activatedRoute : ActivatedRoute) {}
+  constructor(private shopService : ShopService, private activatedRoute : ActivatedRoute, 
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails',' ')
+    }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -23,7 +27,10 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id')
     
     if(id != null) this.shopService.getProduct(+id).subscribe({
-      next : product => this.product = product,
+      next : product => {
+        this.product = product
+        this.bcService.set('@productDetails', product.name)
+      },
       error: error => console.log(error)
     })
   }
