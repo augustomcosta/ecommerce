66f4e826e7b178d08ea73b747022aeeb08f93a29
Augusto Costa <augusto.medeirosdacosta@gmail.com>
2023-12-19 11:53:34 -0300

    feat: create order entities

---
+++ ecommerce-api/Domain/Models/OrderAggregate/DeliveryMethod.cs
@@ -0,0 +1,11 @@
+﻿using ecommerce_api.Domain.Entities.Base;
+
+namespace ecommerce_api.Domain.Models.OrderAggregate;
+
+public class DeliveryMethod : ModelBase
+{
+    public string ShortName { get; set; }
+    public string DeliveryTime { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ ecommerce-api/Domain/Models/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,19 @@
+﻿namespace ecommerce_api.Domain.Models.OrderAggregate;
+
+public class ProductItemOrdered
+{
+    public ProductItemOrdered()
+    {
+    }
+    
+    public ProductItemOrdered(int productItemId, string productName, string imageUrl)
+    {
+        ProductItemId = productItemId;
+        ProductName = productName;
+        ImageUrl = imageUrl;
+    }
+
+    public int ProductItemId { get; set; }
+    public string ProductName { get; set; }
+    public string ImageUrl { get; set; }
+}
