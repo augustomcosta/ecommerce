2d3df45019c5f2127538fffff19f774047a9ac8b
Augusto <augusto.medeirosdacosta@gmail.com>
2024-01-02 10:47:56 -0300

    feat: add submit order feature to checkout

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { DeliveryMethod } from '../shared/models/deliveryMethod';
 import { map } from 'rxjs';
 import { Basket } from '../shared/models/basket';
+import { Order, OrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,10 @@ export class CheckoutService {
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+
+  createOrder(order: OrderToCreate) {
+    return this.http.post<Order>(this.baseUrl + 'orders', order)
+  }
 
   getDeliveryMethods() {
     return this.http.get<DeliveryMethod[]>(this.baseUrl + 'Orders/deliveryMethods').pipe(
